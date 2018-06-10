2d92eda659384a38b3f7c3a8ac06dce6e1a7df0c
Lobarr <jesulobaegunjobi@hotmail.com>
2018-06-09 20:26:53 -0400

    updated color and fixed resume download

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -14,7 +14,7 @@ export default class Info extends Component {
                     <h2>Software Developer.</h2>
                     <p id="info">I'm a curious individual who happens to have a passion for software development and skilled in writing software in Go, Node.js, Python, and C++. I'm a quick learner that loves exploring new technologies and craves challenges that improve me as a developer and increase my skill-set. I am currently pursuing a Bachelors of IT degree in Networking and IT Security at the University of Ontario Institute of Technology (UOIT) and would be graduating in 2020.</p>
                     <div id="resume">
-                        <a  className="" href="../../assets/resume.pdf" target="_blank">
+                        <a  className="" href="https://www.dropbox.com/s/tlpzfe711jchd1u/resume.pdf?dl=0" target="_blank">
                         <span class="icon">
                             <i className="fas fa-download"></i>
                         </span>
